e848cf792cf9025a2d9b8fbf7c656256b7fc305f
kkadete <50638144+kkadete@users.noreply.github.com>
2020-04-12 12:21:25 +0100

    refactor: remove default route

--- kotlin-todomvc-react/src/main/kotlin/components/ApplicationComponent.kt
+++ kotlin-todomvc-react/src/main/kotlin/components/ApplicationComponent.kt
@@ -3,42 +3,22 @@ package katas.todomvc.components
 import katas.todomvc.container.headerComponent
 import katas.todomvc.container.visibleTodoListComponent
 import react.RBuilder
-import react.dom.br
 import react.dom.div
-import react.dom.h1
-import react.dom.section
 import react.router.dom.browserRouter
-import react.router.dom.navLink
 import react.router.dom.route
 import react.router.dom.switch
 
-private const val TODO_LIST_PATH = "/todolist"
+private const val TODO_PATH = "/"
 
 fun RBuilder.application() =
     browserRouter {
         switch {
-            route("/", exact = true) {
-                div {
-                    h1 {
-                        +"Kotlin React + React-Dom + Redux + React-Redux + React-Router Example"
-                    }
-                    navLink(TODO_LIST_PATH) {
-                        +"Go to todo list"
-                    }
-                }
-            }
-            route(TODO_LIST_PATH) {
-                div {
-                    section("application") {
-                        headerComponent {}
-                        visibleTodoListComponent {}
-                        footer()
-                        infoComponent()
-                        br {}
-                        navLink("/") {
-                            +"Go back"
-                        }
-                    }
+            route(TODO_PATH) {
+                div(classes = "application") {
+                    headerComponent {}
+                    visibleTodoListComponent {}
+                    footer()
+                    infoComponent()
                 }
             }
         }
